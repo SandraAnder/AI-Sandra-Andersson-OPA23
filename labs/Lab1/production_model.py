c227173bfa8cd84f2fc089ef7d76e3bfb122125c
Sandra <sandy.andersson.6@gmail.com>
2024-04-25 13:05:56 +0200

    Lab1: .py not compleatly working

--- labs/Lab1/production_model.py
+++ labs/Lab1/production_model.py
@@ -1,26 +1,33 @@
 import pandas as pd
 import joblib
 
-# Ladda in test_samples.csv
-test_samples = pd.read_csv('data/test_samples.csv')
+# Laddar in test_samples med errorhandling
+try:
+    test_samples = pd.read_csv('labs/Lab1/data/test_samples.csv')
+    print("Filen lästes in korrekt.")
+except FileNotFoundError as e:
+    print("Filen kunde inte hittas:", e)
 
-# Ladda in modellen från .pkl-filen
-model = joblib.load('din_modell.pkl')
+# Laddar in min modell från .pkl-filen
+try:
+    model = joblib.load('labs/Lab1/data/svm_model.pkl')
+except FileNotFoundError as e:
+    print("Filen kunde inte hittas:", e)
 
-# Gör förutsägelser på de 100 datapunkterna
+# Gör prediktioner på de 100 datapunkterna
 predictions = model.predict(test_samples)
 
-# Beräkna sannolikheterna för varje klass
+# Beräknar sannolikheterna för varje klass
 probabilities = model.predict_proba(test_samples)
 probability_class_0 = probabilities[:, 0]
 probability_class_1 = probabilities[:, 1]
 
-# Skapa en DataFrame med förutsägelser och sannolikheter
+# Skapar en ny DataFrame med prediktioner och sannolikheter
 prediction_df = pd.DataFrame({
     'probability class 0': probability_class_0,
     'probability class 1': probability_class_1,
     'prediction': predictions
 })
 
-# Exportera DataFrame till CSV
+# Exporterar DataFramen till en CSV-fil
 prediction_df.to_csv('data/prediction.csv', index=False)
